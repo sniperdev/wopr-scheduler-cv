6dd3217ed6bf653d6d5c4f80a2774fefbce81a27
sniperdev <igorspychalaa@gmail.com>
2023-02-27 20:16:04 +0100

    <feat> create nav in main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { IoSend } from "react-icons/all";
 
 const MainPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <nav className="flex m-2">
+        <div className="shadow-lg w-40 p-2 mx-2 rounded-2xl">Ilość godzin:</div>
+        <div className="shadow-lg w-52 p-2 mx-2 rounded-2xl">
+          Wynagrodzenie: 2143 zł
+        </div>
+        <div className="ml-auto w-10 h-10 p-3 mx-4 bg-green-600 rounded-full text-white">
+          <IoSend></IoSend>
+        </div>
+        <div className="shadow-lg w-40 p-2 rounded-2xl text-center">
+          Jan Kowalski
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default MainPage;
