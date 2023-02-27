51120c6b0f54706956eca9a3bf06deabebb2b2fa
sniperdev <igorspychalaa@gmail.com>
2023-02-27 20:27:53 +0100

    <feat> separate nav into component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IoSend } from "react-icons/all";
+import MainNav from "../components/MainNav";
 
 interface props {
   name: string;
@@ -8,16 +8,7 @@ interface props {
 const MainPage = ({ name }: props) => {
   return (
     <>
-      <nav className="flex m-2">
-        <div className="shadow-lg w-40 p-2 mx-2 rounded-2xl">Ilość godzin:</div>
-        <div className="shadow-lg w-52 p-2 mx-2 rounded-2xl">
-          Wynagrodzenie: 2143 zł
-        </div>
-        <div className="ml-auto w-10 h-10 p-3 mx-4 bg-green-600 rounded-full text-white">
-          <IoSend></IoSend>
-        </div>
-        <div className="shadow-lg w-40 p-2 rounded-2xl text-center">{name}</div>
-      </nav>
+      <MainNav name={name} />
     </>
   );
 };
