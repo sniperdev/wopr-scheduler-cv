f88e8536e24af7b64c31555b07534d899cd3c29b
sniperdev <igorspychalaa@gmail.com>
2023-02-27 19:43:16 +0100

    <feat> logi page

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LoginPage = () => {
+	return (
+		<div className="w-screen h-screen flex items-center justify-center">
+				<form className="flex flex-col gap-2 w-1/4">
+					<label>Podaj swoje imie i nazwisko:</label>
+					<input type="text" placeholder="Jan Kowalski" className="border-2 border-black rounded-2xl p-2"/>
+					<button type="submit" className="bg-green-600 text-white rounded-2xl p-2">Akceptuj!</button>
+				</form>
+		</div>
+	);
+};
+
+export default LoginPage;
