03a56cfbe75af3fd7005d8bf6311614452feee93
sniperdev <igorspychalaa@gmail.com>
2023-02-27 20:32:17 +0100

    <feat> prevent user from opening main page without passing name

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainNav from "../components/MainNav";
+import MainCalendar from "../components/MainCalendar";
+import { useNavigate } from "react-router-dom";
 
 interface props {
   name: string;
 }
 
 const MainPage = ({ name }: props) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!name) {
+      navigate("/");
+    }
+  }, []);
   return (
     <>
       <MainNav name={name} />
+      <MainCalendar />
     </>
   );
 };
