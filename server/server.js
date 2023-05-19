2c7e7d3d9d0fc891af91bbcbfdc074fb5553f688
sniperdev <62942349+sniperdev@users.noreply.github.com>
2023-05-19 13:27:05 +0200

    <build> create server folder with basic express server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const db = mysql.createConnection({
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
+	port: process.env.PORT,
+})
+
+app.listen(8800,()=>{
+	console.log("Server is up and running on"+process.env.PORT)
+})
+
+
+db.connect((err)=>{
+	if(err) console.log("Error connecting",err)
+	else console.log("Connected to database")
+})
