241f78f01ddda8527094998084dafc71b9cece9b
sniperdev <62942349+sniperdev@users.noreply.github.com>
2023-05-29 19:10:07 +0200

    <feat> Add getAllDates function to dates controller and add a new route for admin to get all dates.

--- server/controllers/dates.js
+++ server/controllers/dates.js
@@ -11,6 +11,17 @@ export const getDates = (req, res) => {
   });
 };
 
+export const getAllDates = (req, res) => {
+  const q = "SELECT * FROM daty";
+
+  db.query(q, (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0)
+      return res.status(404).json("There is no dates available");
+    if (data.length > 0) return res.status(200).json(data);
+  });
+};
+
 export const addDate = (req, res) => {
   const q =
     "INSERT INTO daty (ID_RATOWNIKA, START, END, ZMIANA) VALUES (?,?,?,?)";
