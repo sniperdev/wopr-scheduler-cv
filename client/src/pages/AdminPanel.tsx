d240adbaa375bb8112be0fafa54246ddfe6ed080
sniperdev <62942349+sniperdev@users.noreply.github.com>
2023-05-29 19:33:23 +0200

    <feat> Add allDates interface and fetch data from server
    This commit adds an interface called allDates to the AdminPanel.tsx file. It also includes a useEffect hook that fetches data from the server using a GET request and sets it to state using setAllDate. The fetched data is then formatted and added to the existing state array.

--- client/src/pages/AdminPanel.tsx
+++ client/src/pages/AdminPanel.tsx
@@ -11,16 +11,59 @@ import FullCalendar from "@fullcalendar/react";
 interface props {
   user: User;
 }
-
+interface allDates {
+  ZMIANA: Number;
+  START: string;
+  END: string;
+  ID_RATOWNIKA: number;
+  IMIE: string;
+  NAZWISKO: string;
+}
 export default function AdminPanel({ user }: props) {
   const navigate = useNavigate();
 
   const [open, setOpen] = useState<boolean>(false);
+  const [allDate, setAllDate] = useState<allDates[]>([]);
 
   useEffect(() => {
     if (user.IMIE.length === 0) {
       navigate("/");
     }
+  }, []);
+
+  useEffect(() => {
+    const fetchAllDates = async () => {
+      await fetch("http://localhost:8800/api/dates/alldatesadmin", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const formattedData = data.map(
+            (element: {
+              ZMIANA: Number;
+              start: string;
+              end: string;
+              ID_RATOWNIKA: number;
+              imie: string;
+              nazwisko: string;
+            }) => ({
+              idRatownika: element.ID_RATOWNIKA,
+              title: element.ZMIANA,
+              imie: element.imie,
+              nazwisko: element.nazwisko,
+              start: element.start,
+              end: element.end,
+            })
+          );
+          setAllDate([...allDate, ...formattedData]);
+          console.log(data);
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchAllDates();
   }, []);
   return (
     <div className="flex h-screen w-screen">
